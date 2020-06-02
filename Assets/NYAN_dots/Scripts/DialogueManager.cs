a7979265e4e27ca2fbc1826a3c22135ac0c2b2a7
qwerty9462 <qwerty9462@gmail.com>
2020-06-02 20:23:55 +0800

    upload

---
+++ Assets/NYAN_dots/Scripts/DialogueManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public GameObject dBox;
+    public Text dText;
+
+    public bool dialogActive;
+    public string[] dialogueLines;
+    public int currentLine;
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        if (dialogActive && Input.GetKeyDown(KeyCode.L)) 
+        {
+
+            // dBox.SetActive(false);
+            //dialogActive = false;
+            currentLine++;
+        }
+        if (currentLine >= dialogueLines.Length)
+        {
+            dBox.SetActive(false);
+            dialogActive = false;
+
+            currentLine = 0;
+        }
+        dText.text = dialogueLines[currentLine];
+    }
+
+    public void ShowBox(string dialogue)
+    {
+        dialogActive = true;
+        dBox.SetActive(true);
+        dText.text = dialogue;
+    }
+
+    public void ShowDialogue()
+    {
+        dialogActive = true;
+        dBox.SetActive(true);
+    }
+}
---
+++ Assets/NYAN_dots/Scripts/dialogHolder.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class dialogHolder : MonoBehaviour
+{
+    public string dialogue;
+    private DialogueManager dMAn;
+    public string[] dialogueLines;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        dMAn = FindObjectOfType<DialogueManager>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                //dMAn.ShowBox(dialogue);
+                if (!dMAn.dialogActive)
+                {
+                    dMAn.dialogueLines = dialogueLines;
+                    dMAn.currentLine = 0;
+                    dMAn.ShowDialogue();
+                }
+            }
+        }
+    }
+}
